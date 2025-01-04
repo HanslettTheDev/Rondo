d5fac5f6de3e4ce13eee3f1b703d31c95ebeb0f8
HanslettTheDev <HanslettTheDev@gmail.com>
2025-01-04 22:09:39 +0100

    feat: login authentication added including the logout feature

--- rondo/auth/routes.py
+++ rondo/auth/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
-from flask_login import current_user, login_required
+from flask_login import current_user, login_required, login_user, logout_user
 
 from rondo.models.roles_permissions import Role
 from rondo.models.users import Users
@@ -8,10 +8,30 @@ from rondo.extensions import db, bcrypt
 
 auth = Blueprint('auth', __name__)
 
+@auth.route("/")
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('admin.dashboard'))
     
-    return render_template('auth/login.html')
+    if request.method == "POST":
+        username = request.form["username"].strip()
+        password = request.form["password"].strip()
+
+        admin = db.session.execute(db.select(Users).filter_by(username=username)).first()
+        if not admin:
+            flash("Password or email incorrect", "danger")
+            return render_template("auth/login.html")
+        
+        if not bcrypt.check_password_hash(admin[0].password, password):
+            flash("Password or email incorrect", "danger")
+            return render_template("auth/login.html")
+        
+        login_user(admin[0])
+        next_page = request.args.get("next")
+        return redirect(next_page) if next_page else redirect(url_for("admin.dashboard"))
+    
+    return render_template("auth/login.html")
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
@@ -46,13 +66,12 @@ def register():
         flash("Account successfully created. Now login", "success")
         return redirect(url_for("auth.login"))
 
-
-        
-
-    return render_template('auth/register.html')
+    return render_template("auth/register.html")
 
 
 @auth.route('/logout')
 @login_required
 def logout():
-    pass
+    logout_user()
+    flash("Logout Successful!", "info")
+    return redirect(url_for("auth.login"))
